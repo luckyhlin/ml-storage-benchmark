afa718076130729732f92f801398132a2c8bda8e
Hao Lin <hlin368@wisc.edu>
2024-12-18 18:12:47 -0600

    add readme

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # do not run bash file directly, but copy and paste the following command to CMDLine instead.
-sudo rm -f profile_output.log && python3 src/test.py & PID=$! && sudo strace -tt -e trace=read,write,open,openat,close -p $PID -o profile_output.log
+sudo rm -f profile_output.log && python3 src/test.py & PID=$! && sudo strace -tt -e trace=read,write,open,openat,close -p $PID -o profile_output.log && sudo chown $USER:$USER profile_output.log
